fceda7838be18944451f2af79fdcda6cae002d48
zeeshan1009 <zeeshan10091009@gmail.com>
2025-04-01 21:50:38 +0500

    chp 5 task complete

---
+++ 5.5._Chapter 5 Practice/Problem_09.py
@@ -0,0 +1,9 @@
+# Can you change the value in the set 's list 
+# S= {12,34,56,78,"Zeehan",{1,2,3}
+
+
+S= {12,34,56,78,"Zeehan",{1,2,3}}
+
+# no this cannot be possible as the values cannot get changed like sets are not indexed 
+# so we cannot access any specific memory locations like they are unorders also they have just one 
+# property which is it have unique memory locations cannot have same members 
