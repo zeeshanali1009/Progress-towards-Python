a7b0e08db73173be622a69b7f45ef04853c8205f
zeeshan1009 <zeeshan10091009@gmail.com>
2025-04-07 23:11:30 +0500

    practice set

---
+++ 11._Chapter 11 Practice/Problem_01.py
@@ -0,0 +1,24 @@
+# This program makes a two dimensional and three dimensional vectors and print their content 
+
+class TwoDVector:
+    def __init__(self, i ,j):
+        self.i = i
+        self.j = j
+    def show(self):
+        print(f"Vector is {self.i}i +  {self.j}j")
+
+class ThreeDVector(TwoDVector):
+    def __init__(self,i,j,k):
+        # super().__init__(i,j)
+        self.i = i
+        self.j = j
+        self.k = k
+    def show(self):
+        print(f"Vector is {self.i}i + {self.j}j + {self.k}k")
+   
+
+a = TwoDVector(1,2)
+a.show()
+b= ThreeDVector(1,2,3)
+b.show()
+
